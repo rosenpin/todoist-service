89077484a72f461fbd01a28bcf7a8dcbe0aa223a
rosenpin <mail@tomerrosenfeld.com>
2022-12-06 23:03:02 +0200

    hash correctly

--- todoist_service/server/authorization/registration.py
+++ todoist_service/server/authorization/registration.py
@@ -1,10 +1,11 @@
 from todoist_service.db import DB
 from todoist_service.todoist_wrapper.todoist_api_wrapper import TodoistAPIWrapper
 
+import hashlib
 
 def register_user(db: DB, access_token: str):
     doist = TodoistAPIWrapper(access_token)
-    user_id = hash(access_token)
+    user_id = hashlib.md5(access_token.encode('utf-8')).hexdigest()
     user_name = "John Doe"
 
     db.add_user(user_id=user_id, token=access_token, full_name=user_name)
